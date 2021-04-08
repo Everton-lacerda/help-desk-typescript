4d7cb7845032c1f4887aa167ccf7a0ef4f0eb7d1
blueeverton <everton@bluelogic.com.br>
2021-04-08 20:38:37 -0300

    add  styled comp, config styles global and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'; 
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from "./styles/GlobalStyles";
+import dark from './styles/themes/dark';
+import light from './styles/themes/light';
 
 function App() {
   return (
     <div>
-      <h1>Help Desk</h1>
+      <ThemeProvider theme={light}>
+        <GlobalStyles />
+        <h1>Página Inicial</h1>
+      </ThemeProvider>
     </div>
   );
 }
